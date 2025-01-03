658831f03ae1a1c9cfccbb0dc6efe1346298c9b3
Shalia <shalia.wynn730@gmail.com>
2025-01-03 17:55:48 -0600

    adding modifications. not sure what changed

--- day08/day08.py
+++ day08/day08.py
@@ -70,28 +70,25 @@ with open("input.txt") as f_handle:
 		left_element = left_element.strip().replace("(", "")
 		right_element = right_element.strip().replace(")", "")
 		node_mappings[node] = (left_element, right_element)
-	# The main algorithm.
-	is_first_pass = True
+	# The main algorithm. Solved using LCM (Least Common Mutipl)
 	starting_nodes = [node for node in nodes if node.endswith("A")]
 	starting_nodes_are_transformed = [False for x in range(len(starting_nodes))]
 	while not all(starting_nodes_are_transformed):
 		for instruction in instructions:
 			for starting_node_index, starting_node in enumerate(starting_nodes):
-				next_node = ""
 				if starting_node.endswith("Z"):
 					starting_nodes_are_transformed[starting_node_index] = True
-				else:
+				elif not starting_node.endswith("Z"):
 					starting_nodes_are_transformed[starting_node_index] = False
+
+				next_node = ""
 				if instruction == "L":
 					next_node = node_mappings[starting_node][0]
 				else:
 					next_node = node_mappings[starting_node][1]
 				starting_nodes[starting_node_index] = next_node
-			if is_first_pass:
-				num_steps += 0
-				is_first_pass = False
-			else:
-				num_steps += 1
+
 			if all(starting_nodes_are_transformed):
-					break
+				break
+			num_steps += 1
 print("The number of steps required to reach all the nodes that only end with 'Z' are", num_steps)
